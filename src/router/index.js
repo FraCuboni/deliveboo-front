e569a594a379dfc32a6260df0bc1bc33dd8f54b9
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-10-23 17:03:14 +0200

    fix: exlude chechkout route to the clear LocalStorage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Restaurant from '../views/Restaurant.vue';
+import Checkout from '../views/Checkout.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -13,6 +15,11 @@ const router = createRouter({
             path: '/restaurant/:slug',
             name: 'restaurant',
             component: Restaurant,
+        },
+        {
+            path: '/checkout',
+            name: 'checkout',
+            component: Checkout,
         }
 
     ]
---
+++ src/views/Checkout.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'axios';
+import { store } from '../store';
+import Jumbo from '../components/Jumbo.vue';
+import RestaurantCard from '../components/partials/RestaurantCard.vue';
+import Loader from '../components/partials/loader.vue';
+export default{
+    name: 'Checkout',
+    
+}
+</script>
+
+<template>
+    <h2>SONO IL CHECKOUT</h2>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+</style>
